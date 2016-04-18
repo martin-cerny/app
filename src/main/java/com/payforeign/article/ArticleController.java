a88ee88d9a8eebbc2b7978cfc8a3ec84db49faf3
Martin Cerny <martin.cerny@affiliatewindow.com>
2016-04-19 00:07:48 +0100

    add thymeleaf

--- src/main/java/com/payforeign/article/ArticleController.java
+++ src/main/java/com/payforeign/article/ArticleController.java
@@ -2,6 +2,7 @@ package com.payforeign.article;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,8 +16,13 @@ public class ArticleController {
     
     @Autowired
     private ArticleRepository repository;
-
+    
     @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> service() {
+        return ResponseEntity.ok("service!");
+    }
+
+    @RequestMapping(path = "/index",method = RequestMethod.GET)
     public String index() {
         return "article/index";
     }
